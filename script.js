f8f3825d317ba30b506eecb0451a0d3951c56d7d
Zahra A <zahraa@Zahras-Air.home>
2022-11-10 22:40:25 -0500

    walls/path setup

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(() => {
 
-    
+
 
 
 
 
 
 });
+
+//make it so the user is safe on black <div path>, <div wall2> is a black wall accidentally so try to let user pass through that too ?
+//game ends when user touches turquoise squares
