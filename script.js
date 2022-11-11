22b260b724a072bf1c891416e284283c3fba763b
Zahra A <zahraa@Zahras-Air.home>
2022-11-11 11:33:59 -0500

    redesigned

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $(() => {
 
 document.addEventListener('mousemove', onMouseMove);
 
-
 })
 
 //make it so the user is safe on black <div path>, <div wall2> is a black wall accidentally so try to let user pass through that too ?
