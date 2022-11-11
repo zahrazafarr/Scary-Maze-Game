19cfe88ccf78eaf787b6471d6f8e69d5ec747cff
Zahra A <zahraa@Zahras-Air.home>
2022-11-10 23:08:41 -0500

    player/start/end created

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(() => {
 
+    let circle = document.getElementById('player');
+    const onMouseMove = (e) =>{
+    circle.style.left = e.pageX + 'px';
+    circle.style.top = e.pageY + 'px';
+}
+
+document.addEventListener('mousemove', onMouseMove);
 
 
-
-
-
-
-});
+})
 
 //make it so the user is safe on black <div path>, <div wall2> is a black wall accidentally so try to let user pass through that too ?
 //game ends when user touches turquoise squares
